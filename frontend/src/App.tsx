b0c77681dc4a88dd35455e26a729ebd0ef230821
jvoliveirag <jv.oliveira@gmail.com>
2023-11-14 02:34:09 -0300

    feat: Adicionado o frontend da aplicação.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,125 @@
+import { useCompletion } from "ai/react";
+import { Github, Wand2 } from "lucide-react";
+import { useState } from "react";
+import { PromptSelect } from "./components/prompt-select";
+import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Separator } from "./components/ui/separator";
+import { Slider } from "./components/ui/slider";
+import { Textarea } from "./components/ui/textarea";
+
+export function App() {
+
+  const [temperature, setTemperature] = useState(0.5)
+  
+  const { input, setInput, handleInputChange, handleSubmit, completion, isLoading } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      temperature,
+    },
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <div className="flex items-center">
+          <img src="favicon.png" alt="logo" width={35} className="flex flex-1"/>
+          <h1 className="text-2xl font-bold flex ml-4">
+            Woodie.ai
+          </h1>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido para o TCC do <a href="https://www.inatel.br/" className="underline font-bold">Inatel</a>
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            <a href="https://github.com/jvoliveirag/TCC">Github</a>
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
+            />
+            <Textarea 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..." 
+              readOnly
+              value={completion}
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Desenvolvido por <code className="text-violet-400">{'{João Victor de Oliveira}'}</code> - Cachoeira de Minas, 2023. Todos os direitos reservados.
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <PromptSelect onPromptSelected={setInput} />
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="woodie">
+                <SelectTrigger>
+                  <SelectValue/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5 turbo 16k</SelectItem>
+                  <SelectItem value="woodie">ft-woodie.ai-robot-design</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator/>
+
+            <div className="space-y-4">
+              <Label>Temperatura: <span className="border border-slate-800 rounded-sm px-1 text-base">{temperature}</span></Label>
+              <Slider
+                className="cursor-pointer"
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
+              />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo (porém com possíveis erros).
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button disabled={isLoading} type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2"/>
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  )
+}
