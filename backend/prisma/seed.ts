a7bf38c27f9b94db9a51dff545cfa8b6e62eb202
jvoliveirag <jv.oliveira@gmail.com>
2023-11-29 02:06:33 -0300

    refactor: Ajuste no banco de dados (orm).

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -6,80 +6,22 @@ async function main() {
 
   await prisma.prompt.create({
     data: {
-      title: 'Título YouTube',
-      template: `Seu papel é gerar três títulos para um vídeo do YouTube.
-
-Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar os títulos.
-Abaixo você também receberá uma lista de títulos, use essa lista como referência para os títulos a serem gerados.
-
-Os títulos devem ter no máximo 60 caracteres.
-Os títulos devem ser chamativos e atrativos para maximizar os cliques.
-
-Retorne APENAS os três títulos em formato de lista como no exemplo abaixo:
-'''
-- Título 1
-- Título 2
-- Título 3
-'''
-
-Transcrição:
-'''
-{transcription}
-'''`.trim()
+      title: 'Navegação do robô',
+      template: `Como posso fazer para melhorar a navegação do meu robô no tapete da FLL?`.trim()
     }
   })
 
   await prisma.prompt.create({
     data: {
-      title: 'Descrição YouTube',
-      template: `Seu papel é gerar uma descrição sucinta para um vídeo do YouTube.
-  
-Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar a descrição.
-
-A descrição deve possuir no máximo 80 palavras em primeira pessoa contendo os pontos principais do vídeo.
-
-Use palavras chamativas e que cativam a atenção de quem está lendo.
-
-Além disso, no final da descrição inclua uma lista de 3 até 10 hashtags em letra minúscula contendo palavras-chave do vídeo.
-
-O retorno deve seguir o seguinte formato:
-'''
-Descrição.
-
-#hashtag1 #hashtag2 #hashtag3 ...
-'''
-
-Transcrição:
-'''
-{transcription}
-'''`.trim()
+      title: 'Tipos de roda',
+      template: `Como posso escolher o melhor tipo de roda para meu robô?`.trim()
     }
   })
 
   await prisma.prompt.create({
     data: {
-      title: 'Post para LinkedIn',
-      template: `Seu papel é gerar uma descrição sucinta para um vídeo que será postado no LinkedIn.
-  
-Abaixo você receberá uma transcrição desse vídeo, use essa transcrição para gerar a descrição.
-
-A descrição deve possuir no máximo 200 palavras em primeira pessoa contendo os pontos principais do vídeo.
-
-Use palavras chamativas e que cativam a atenção de quem está lendo.
-
-Além disso, no final da descrição inclua uma lista de 3 até 10 hashtags em letra minúscula contendo palavras-chave do vídeo.
-
-O retorno deve seguir o seguinte formato:
-'''
-Descrição.
-
-#hashtag1 #hashtag2 #hashtag3 ...
-'''
-
-Transcrição:
-'''
-{transcription}
-'''`.trim()
+      title: 'Estrutura',
+      template: `Como desenvolver a melhor estrutura para o meu robô`.trim()
     }
   })
 }
